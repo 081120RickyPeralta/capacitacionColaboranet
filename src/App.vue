203a3c752bb96f49ad537baabb03dfd7d70ce76d
RICKY <081120RickyPeralta>
2024-07-26 10:09:56 -0600

    implementacion de JSON con input padres y varios hijos

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,119 @@
 <template>
   <div class="container mt-5">
     <form>
-      <div class="mb-3">
-        <label for="country" class="form-label">Selecciona un país:</label>
-        <select v-model="selectedCountry" @change="checkOtherCountry" class="form-select">
-          <option v-for="country in countries" :key="country.value" :value="country.value">
-            {{ country.name }}
-          </option>
-        </select>
-      </div>
+      <div v-for="field in fields" :key="field.model" class="mb-3">
+        <label :for="field.model" class="form-label">{{ field.label }}:</label>
 
-      <div v-if="showOtherCountryInput" class="mb-3">
-        <label for="otherCountry" class="form-label">Especifica otro país:</label>
-        <input type="text" v-model="otherCountry" id="otherCountry" class="form-control" />
+        <template v-if="!field.type || field.type === 'select'">
+          <select
+            v-model="formData[field.model]"
+            @change="checkCondition(field)"
+            class="form-select"
+          >
+            <option v-for="option in field.options" :key="option.value" :value="option.value">
+              {{ option.name }}
+            </option>
+          </select>
+        </template>
+
+        <template v-if="field.type === 'number'">
+          <input
+            :type="field.type"
+            v-model="formData[field.model]"
+            @change="checkCondition(field)"
+            :id="field.model"
+            class="form-control"
+          />
+        </template>
+
+        <div v-if="showExtraInput[field.model]" class="mt-3">
+          <template v-if="field.extraInputs">
+            <div v-for="extraInput in field.extraInputs" :key="extraInput.model" class="mb-3">
+              <label :for="extraInput.model" class="form-label">{{ extraInput.label }}:</label>
+              <input
+                type="text"
+                v-model="formData[extraInput.model]"
+                :id="extraInput.model"
+                class="form-control"
+              />
+            </div>
+          </template>
+
+          <template v-else>
+            <label :for="field.extraInputModel" class="form-label"
+              >{{ field.extraInputLabel }}:</label
+            >
+            <input
+              type="text"
+              v-model="formData[field.extraInputModel]"
+              :id="field.extraInputModel"
+              class="form-control"
+            />
+          </template>
+        </div>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import countriesData from './validacion.json';
+import { ref, reactive, onMounted } from 'vue';
+import formRules from './formRules.json';
 
 export default {
-  data() {
-    return {
-      selectedCountry: '',
-      showOtherCountryInput: false,
-      otherCountry: '',
-      countries: countriesData.countries,
+  setup() {
+    const formData = reactive({});
+    const showExtraInput = reactive({});
+    const fields = ref(formRules.fields);
+
+    const initializeForm = () => {
+      fields.value.forEach((field) => {
+        formData[field.model] = '';
+        if (field.extraInputs) {
+          field.extraInputs.forEach((extraInput) => {
+            formData[extraInput.model] = '';
+          });
+        } else {
+          formData[field.extraInputModel] = '';
+        }
+        showExtraInput[field.model] = false;
+      });
     };
-  },
-  methods: {
-    checkOtherCountry() {
-      const selected = this.countries.find((country) => country.value === this.selectedCountry);
-      this.showOtherCountryInput = selected ? selected.requiresInput : false;
-      if (!this.showOtherCountryInput) {
-        this.otherCountry = '';
+
+    const checkCondition = (field) => {
+      const value = formData[field.model];
+      const option = field.options ? field.options.find((opt) => opt.value === value) : null;
+      const condition = field.condition;
+
+      if (option && option.requiresInput) {
+        showExtraInput[field.model] = true;
+      } else if (condition && condition.lessThan !== undefined) {
+        showExtraInput[field.model] = value < condition.lessThan;
+      } else {
+        showExtraInput[field.model] = false;
       }
-    },
+
+      if (!showExtraInput[field.model]) {
+        if (field.extraInputs) {
+          field.extraInputs.forEach((extraInput) => {
+            formData[extraInput.model] = '';
+          });
+        } else {
+          formData[field.extraInputModel] = '';
+        }
+      }
+    };
+
+    onMounted(() => {
+      initializeForm();
+    });
+
+    return {
+      formData,
+      showExtraInput,
+      fields,
+      checkCondition,
+    };
   },
 };
 </script>
